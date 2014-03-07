3e8670c7b0aae06fee91b94ed2bcfda6566977bb
Ryan Dougherty <ryand626@gmail.com>
2014-03-06 23:27:50 -0500

    Illusion of choice
    script in main menu scene can register that the mouse is over a game
    object via raycasting.  I will try to make it transition to a level next

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+	private Ray mouseRay;
+	private RaycastHit hit;
+
+	public Camera menuCam;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mouseRay = menuCam.ScreenPointToRay(Input.mousePosition);
+		if (Physics.Raycast (mouseRay, out hit, 50)) {
+			if (hit.collider.name == "choice1"){
+				print("choice 1");
+			}
+			if (hit.collider.name == "choice2"){
+				print("choice 2");
+			}
+		}
+	}
+}
